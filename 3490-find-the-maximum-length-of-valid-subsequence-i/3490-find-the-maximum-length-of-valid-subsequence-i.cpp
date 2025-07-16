b2b57755291b6182bc462f9afdc017c2ed50c1d2
Sonam Nimje <148960992+sonamnimje@users.noreply.github.com>
2025-07-16 22:16:39 +0530

    Time: 11 ms (32.94%), Space: 96.3 MB (30%) - LeetHub

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int even = 0, odd = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) even++;
+            else odd++;
+        }
+
+        // Case 3: Find longest alternating parity subsequence
+        int alt = 1;
+        int prev = nums[0] % 2;
+        for (int i = 1; i < nums.size(); ++i) {
+            int curr = nums[i] % 2;
+            if (curr != prev) {
+                alt++;
+                prev = curr;
+            }
+        }
+
+        return max({even, odd, alt});
+    }
+};
