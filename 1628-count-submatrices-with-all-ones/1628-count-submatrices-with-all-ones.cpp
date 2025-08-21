fa8d7a282c39b24b50556666de5f3bead871d965
Sonam Nimje <148960992+sonamnimje@users.noreply.github.com>
2025-08-21 23:16:23 +0530

    Time: 0 ms (100%), Space: 18.6 MB (66.25%) - LeetHub

---
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numSubmat(vector<vector<int>>& mat) 
+    {
+       int m = mat.size(), n = mat[0].size();
+        vector<int> height(n, 0);
+        int result = 0;
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                if (mat[i][j] == 0) height[j] = 0;
+                else height[j] += 1;
+            }
+            for (int j = 0; j < n; j++) 
+            {
+                int minH = height[j];
+                for (int k = j; k >= 0 && minH > 0; k--) 
+                {
+                    minH = min(minH, height[k]);
+                    result += minH;
+                }
+            }
+        }
+        return result; 
+    }
+};
