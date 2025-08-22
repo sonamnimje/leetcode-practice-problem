46fff2288296ef8747489673ac40a35ebf299504
Sonam Nimje <148960992+sonamnimje@users.noreply.github.com>
2025-08-22 19:49:22 +0530

    Time: 235 ms (95.21%), Space: 133.5 MB (72.52%) - LeetHub

---
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumArea(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int minRow = m, maxRow = -1;
+        int minCol = n, maxCol = -1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    minRow = min(minRow, i);
+                    maxRow = max(maxRow, i);
+                    minCol = min(minCol, j);
+                    maxCol = max(maxCol, j);
+                }
+            }
+        }
+        if (maxRow == -1) return 0;
+
+        int height = maxRow - minRow + 1;
+        int width = maxCol - minCol + 1;
+
+        return height * width;
+    }
+};
