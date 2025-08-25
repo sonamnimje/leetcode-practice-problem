1931bbaac67dce16bfd943b1af558afbc6ca12d7
Sonam Nimje <148960992+sonamnimje@users.noreply.github.com>
2025-08-25 14:36:41 +0530

    Time: 4 ms (81.69%), Space: 11.2 MB (33.82%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n = s.size();
-        vector<int> count(3, 0); // counts for a, b, c
-        int left = 0, right = 0;
-        int result = 0;
+        int n =s.size();
+        vector<int>count(3,0);
+        int left=0, right=0;
+        int result=0;
 
-        while (right < n) {
-            count[s[right] - 'a']++;  
-            // shrink window while it still has all 3 chars
-            while (count[0] > 0 && count[1] > 0 && count[2] > 0) 
-            {
-                result += n - right;        // all substrings starting at left are valid
-                count[s[left] - 'a']--;  // remove s[left] and move left
-                left++;
-            }
-
-            right++;  // expand window
+        while(right<n)
+        {
+            count[s[right]-'a']++;
+            while(count[0]>0 && count[1]>0 && count[2]>0)
+        {
+            result += n-right;
+            count[s[left]-'a']--;
+            left++;
         }
-
+        right++;
+        }
         return result;
     }
 };
